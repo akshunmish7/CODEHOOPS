d08739a2c84ec3db33efc58e019dc04aacfcc583
Akshun Mishra <akshun@Akshuns-MacBook-Air.local>
2023-05-23 16:13:13 +0530

    Assignment of adding posts controller done

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const homeController=require('../controllers/home_controller');
 
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'))
+
+//for any furthetr routes access from here
+// router.use('/routerName, require('./routerfile));
 
 module.exports=router;
